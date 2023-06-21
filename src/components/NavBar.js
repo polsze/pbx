8165ada9019be9ada515d7da368877689831cdbf
polsze <polbarrios835237@gmail.com>
2023-06-21 15:01:17 -0300

    fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ const CustomLink = ({ href, title, className = '' }) => {
 }
 
 const CustomMobileLink = ({ href, title, className = '', toggle }) => {
-	const router = usePathname();
+	const router = useRouter();
 
 	const handleClick = () => {
 		toggle();
@@ -53,7 +53,7 @@ const NavBar = () => {
 		<header
 			className='w-full px-32 py-8 font-medium flex items-center justify-between relative z-10 lg:px-16 md:px-12 sm:px-8'
 		>
-			<button className='flex-col justify-center items-center hidden lg:flex bg-lime-500' onClick={handleClick}>
+			<button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
 				<span className={`bg-light  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
 				<span className={`bg-light  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
 				<span className={`bg-light  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
