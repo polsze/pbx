0d15a549465bbdb979f531df6eeae16499b97d55
polsze <polbarrios835237@gmail.com>
2023-07-31 16:36:21 -0300

    adding service page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import React from 'react'
 import Project1 from '../../public/images/projects/hs.webp'
 import Project2 from '../../public/images/projects/project2.webp'
+import Project3 from '../../public/images/projects/sa.png'
 import { motion } from 'framer-motion'
 import TransitionEffect from '@/components/TransitionEffect'
 import Cubes from '../../public/images/profile/bg.webp'
@@ -70,7 +71,32 @@ const FeaturedProject2 = ({ type, title, summary, img, link, github }) => {
   )
 }
 
+const FeaturedProject3 = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className='w-full flex items-center justify-between rounded-br-2xl rounded-3xl border border-solid border-dark
+     bg-light shadow-2xl p-12 relative lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
+     '>
+      <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl'></div>
+      <Link href={link} target={"_blank"} className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'>
+        <FramerImage src={img} alt={title} className='w-full h-auto' whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }} />
+      </Link>
 
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+        <span className='text-primary font-medium text-xl xs:text-base'>{type}</span>
+        <Link href={link} target={"_blank"} className='hover:underline underline-offset-2'>
+          <h2 className='my-2 w-full text-left text-4xl font-bold sm:text-sm'>{title}</h2>
+        </Link>
+        <p className='my-2 font-medium text-dark'>{summary}</p>
+        <div className='mt-2 flex items-center'>
+          <Link href="https://www.github.com/polsze" target={"_blank"} className='w-10'><GithubIcon /></Link>
+          <a href="https://serviceautorizado.vercel.app/" target={"_blank"} className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base'>Visitá la página</a>
+        </div>
+      </div>
+
+    </article>
+  )
+}
 
 const projects = () => {
   return (
@@ -98,15 +124,26 @@ const projects = () => {
               />
             </div>
             <div className='col-span-12'>
-              <FeaturedProject2
-                title="Yarara en Acción"
-                img={Project2}
-                summary="Página web para una asociación orientada en nuestra fauna y su protección, dedicada al rescate y la liberación de animales silvestres."
-                link="https://yararaenaccion.vercel.app/"
-                github="github.com/polsze"
+              <FeaturedProject3
+                title="Service Autorizado"
+                img={Project3}
+                summary="Página web para un Servicio Técnico de Helaras de Capital Federal, Buenos Aires."
+                link="https://serviceautorizado.vercel.app/"
+                github="github.com/polsze/serviceautorizado"
                 type="Proyecto en Desarrollo"
               />
             </div>
+            <div className='col-span-12'>
+              <FeaturedProject2
+                title="Yarará en Acción"
+                img={Project2}
+                summary="Página web para una asociación orientada en nuestra fauna y su protección, dedicada al rescate y la liberación de animales silvestres."
+                link="https://yararaenaccion.vercel.app/"
+                github="github.com/polsze/yarara"
+                type="Proyecto en Desarrollo"
+              />
+            </div>
+             
           </div>
         </Layout>
         <div className='absolute right-32 bottom- inline-block w-96 md:hidden 2xl:hidden '>
